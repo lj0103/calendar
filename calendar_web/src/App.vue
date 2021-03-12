2b0204e798bb0bc014c0b8fca8b9693f84658aef
卟噜卟噜叭 <591274912@qq.com>
2021-03-12 15:55:02 +0800

    header组件

--- calendar_web/src/App.vue
+++ calendar_web/src/App.vue
@@ -1,4 +1,24 @@
 <template>
-  <router-view/>
+  <div id="app">
+    <my-header>{{ headerTitle }}</my-header>
+    <router-view />
+  </div>
 </template>
+<script>
+import MyHeader from "@/components/header";
 
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  name: "App",
+  components: {
+    MyHeader,
+  },
+  setup() {
+    const store = useStore(),
+          state = store.state;
+
+    return computed(() => state).value;
+  },
+};
+</script>
